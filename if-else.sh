3ca62949bbe212b87df7a5e4372ac5a04ed80414
mingjue <zhening.czn@alibaba-inc.com>
2019-09-27 18:28:46 +0800

    update

---
+++ if-else.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#if
+num=3
+if [[ num>1 ]]; then 
+	echo "${num} 大于 1"
+fi
+
+#if else
+num=17
+
+if [[ num>=17 ]]; then
+	echo "成年了你"
+else
+	echo " 还未成年"
+fi
+
+#if else if else
+
+
+
--- string.sh
+++ string.sh
@@ -3,9 +3,21 @@
 name="程序亦非猿"
 
 #单引号不能引用变量
-str='a simple ``string ${name}'
+str='a simple ``string ${name}' #a simple ``string ${name}
 echo ${str}
 
-str="Hello,${name},\"大风起兮云飞扬\"!"
+str="Hello,${name},\"大风起兮云飞扬\"!" #Hello,程序亦非猿,"大风起兮云飞扬"!
 
 echo ${str}
+
+#获取字符串的长度
+str="abc"
+echo "${str} 的长度为 ${#str}" #abc 的长度为 3
+
+#提取字符串
+str="abcdef"
+echo "${str:1:2}" #bc
+echo "${str:3}" #def
+
+str="abcdef"
+echo ""
